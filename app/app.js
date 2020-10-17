12f1eb3d5b1f24c64b4f8945869570bc7c7e28f3
dineshsuvendiran <dineshsuvendiran@gmail.com>
2020-10-17 21:59:58 +0530

    bundles working fine with all the modules

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
   "use strict";
   import './klondike/scoring'
+  import './klondike/klondike'
+  import './klondike/board'
+  import './klondike/game'
 
   angular.module("solitaire", ["klondike", "ngDraggable"]);
   
